87beebba8813dbfec9629d7f2dac82281452348a
Corgi239 <89700445+Corgi239@users.noreply.github.com>
2022-02-19 01:40:12 +0200

    Created a simple Drag-and-Drop script

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    private Camera _mainCamera;
+    private bool _isDragging;
+    private Vector3 _grabPoint;
+
+
+    public void Awake()
+    {
+        _mainCamera = Camera.main;
+    }
+
+    public void OnMouseDown()
+    {
+        _isDragging = true;
+        _grabPoint = _mainCamera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+    }
+
+    public void OnMouseUp()
+    {
+        _isDragging = false;
+    }
+
+    public void Update()
+    {
+        if (_isDragging)
+        {
+            Vector2 mousePosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition) - _grabPoint - transform.position;
+            transform.Translate(mousePosition);
+        }
+    }
+}
