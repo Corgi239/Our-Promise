2de9b5f3115a58a509dd8ed3d55ffc762fddd463
Corgi239 <89700445+Corgi239@users.noreply.github.com>
2022-06-12 15:27:45 +0300

    Add scene transitions

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -6,10 +6,20 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    [SerializeField] private String targetSceneName;
+    [SerializeField] private Animator transition;
+    public float transitionTime = 1f;
+    private static readonly int Start = Animator.StringToHash("Start");
 
-    public void OnMouseUpAsButton()
+    public void ToNextScene(string targetScene)
     {
-        SceneManager.LoadScene(targetSceneName);
+        transition.speed = 1 / transitionTime;
+        StartCoroutine(LoadSceneWithTransition(targetScene));
+    }
+
+    private IEnumerator LoadSceneWithTransition(string targetScene)
+    {
+        transition.SetTrigger(Start);
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(targetScene);
     }
 }
